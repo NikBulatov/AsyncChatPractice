f7255e6d9005fd95b1c264399794819b6f48d1f8
Nikita Bulatov <nikita_bulatov@icloud.com>
2023-05-03 21:36:05 +0300

    reduce copypaste

--- lesson_5/unit_tests/test_utils.py
+++ lesson_5/unit_tests/test_utils.py
@@ -1,94 +1,64 @@
-"""Unit-тесты утилит"""
-
-import sys
-import os
-import unittest
 import json
-sys.path.append(os.path.join(os.getcwd(), '..'))
-from common.variables import RESPONSE, ERROR, USER, ACCOUNT_NAME, TIME, ACTION, PRESENCE, ENCODING
-from common.utils import get_message, send_message
+import unittest
+import sys
+
+sys.path.append("../")
+
+from extensions import (RESPONSE, ERROR, USER, ACCOUNT_NAME, TIME, ACTION,
+                        PRESENCE, ENCODING,
+                        get_message, send_message)
+
 
 class TestSocket:
-    '''
-    Тестовый класс для тестирования отправки и получения,
-    при создании требует словарь, который будет прогонятся
-    через тестовую функцию
-    '''
     def __init__(self, test_dict):
         self.test_dict = test_dict
-        self.encoded_message = None
-        self.receved_message = None
+        self.encoded_msg = None
+        self.received_msg = None
 
     def send(self, message_to_send):
-        """
-        Тестовая функция отправки, корретно  кодирует сообщение,
-        так-же сохраняет что должно было отправлено в сокет.
-        message_to_send - то, что отправляем в сокет
-        :param message_to_send:
-        :return:
-        """
-        json_test_message = json.dumps(self.test_dict)
-        # кодирует сообщение
-        self.encoded_message = json_test_message.encode(ENCODING)
-        # сохраняем что должно было отправлено в сокет
-        self.receved_message = message_to_send
+        json_test_msg = json.dumps(self.test_dict)
+        self.encoded_msg = json_test_msg.encode(ENCODING)
+        self.received_msg = message_to_send
 
     def recv(self, max_len):
-        """
-        Получаем данные из сокета
-        :param max_len:
-        :return:
-        """
-        json_test_message = json.dumps(self.test_dict)
-        return json_test_message.encode(ENCODING)
+        json_msg = json.dumps(self.test_dict)
+        return json_msg.encode(ENCODING)
 
 
 class Tests(unittest.TestCase):
-    '''
-    Тестовый класс, собственно выполняющий тестирование.
-    '''
     test_dict_send = {
         ACTION: PRESENCE,
-        TIME: 111111.111111,
+        TIME: 123545.123,
         USER: {
-            ACCOUNT_NAME: 'test_test'
+            ACCOUNT_NAME: "test_name"
         }
     }
     test_dict_recv_ok = {RESPONSE: 200}
     test_dict_recv_err = {
         RESPONSE: 400,
-        ERROR: 'Bad Request'
+        ERROR: "Bad Request"
     }
 
     def test_send_message(self):
-        """
-        Тестируем корректность работы фукции отправки,
-        создадим тестовый сокет и проверим корректность отправки словаря
-        :return:
-        """
-        # экземпляр тестового словаря, хранит собственно тестовый словарь
         test_socket = TestSocket(self.test_dict_send)
-        # вызов тестируемой функции, результаты будут сохранены в тестовом сокете
-        send_message(test_socket, self.test_dict_send)
-        # проверка корретности кодирования словаря.
-        # сравниваем результат довренного кодирования и результат от тестируемой функции
-        self.assertEqual(test_socket.encoded_message, test_socket.receved_message)
-        # дополнительно, проверим генерацию исключения, при не словаре на входе.
+        test_socket.send(self.test_dict_send)
+        self.assertEqual(eval(test_socket.encoded_msg.decode(ENCODING)),
+                         test_socket.received_msg)
         with self.assertRaises(Exception):
-            send_message(test_socket, test_socket)
+            json_msg = json.dumps(test_socket)
+            test_socket.send(json_msg)
 
     def test_get_message(self):
-        """
-        Тест функции приёма сообщения
-        :return:
-        """
         test_sock_ok = TestSocket(self.test_dict_recv_ok)
         test_sock_err = TestSocket(self.test_dict_recv_err)
-        # тест корректной расшифровки корректного словаря
-        self.assertEqual(get_message(test_sock_ok), self.test_dict_recv_ok)
-        # тест корректной расшифровки ошибочного словаря
-        self.assertEqual(get_message(test_sock_err), self.test_dict_recv_err)
+
+        resp_ok = test_sock_ok.recv(4096)
+        self.assertEqual(eval(resp_ok.decode(ENCODING)), self.test_dict_recv_ok)
+
+        resp_err = test_sock_err.recv(4096)
+        self.assertEqual(eval(resp_err.decode(ENCODING)),
+                         self.test_dict_recv_err)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
