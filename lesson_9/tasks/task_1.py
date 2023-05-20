df16efc4f989fa4fc2673be03689cde34f7e82ae
Nikita Bulatov <nikita_bulatov@icloud.com>
2023-05-20 16:28:05 +0300

    refactoring task 1

--- lesson_9/tasks/task_1.py
+++ lesson_9/tasks/task_1.py
@@ -3,9 +3,23 @@ from subprocess import Popen, PIPE
 from socket import gethostbyname
 from ipaddress import IPv4Address, ip_address
 from platform import system
+from typing import Iterable
 
 
-def get_ip_addr(domain: str) -> str | IPv4Address:
+def _ping(host, timeout: int = 250, amount: int = 1) -> Popen:
+    """
+    Ping command creator
+    :param host:
+    :param timeout:
+    :param amount:
+    :return:
+    """
+    param = '-n' if system().lower() == 'windows' else '-c'
+    process = Popen(f"ping {host} -w {timeout} {param} {amount}", stdout=PIPE)
+    return process
+
+
+def _get_ip_addr(domain: str) -> str | IPv4Address:
     """
     Function parses received string containing IP address or domain name
     and returns string containing domain name or IPv4address object
@@ -14,40 +28,35 @@ def get_ip_addr(domain: str) -> str | IPv4Address:
     """
     try:
         ip_addr = ip_address(gethostbyname(domain))
-    except ValueError:
+    except (ValueError, TypeError):
         ip_addr = domain
 
     return ip_addr
 
 
-def host_ping(endpoints: list[str], timeout: int = 500,
-              amount: int = 1) -> dict:
+def host_ping(endpoints: Iterable[str | IPv4Address]) -> dict:
     """
     Function receives a list of strings containing IP address,
     timeout connection and amount requests.
     It returns a dict with results of ping command
     :param endpoints:
-    :param timeout:
-    :param amount:
     :return:
     """
-    results = {0: "", 1: ""}
+    results = {"reachable": [], "unreachable": []}
 
-    param = '-n' if system().lower() == 'windows' else '-c'
     for addr in endpoints:
-        if isinstance(addr, str):
-            host = get_ip_addr(addr)
-            command = f"ping {host} -w {timeout} {param} {amount}"
-            process = Popen(command, stdout=PIPE)
-            process.wait()
-
-            if process.returncode == 0:
-                results[0] += f"{addr}\n"
-                res_output = f"{addr} - Узел доступен"
-            else:
-                results[1] += f"{addr}\n"
-                res_output = f"{addr} - Узел не доступен"
-            print(res_output)
+        host = _get_ip_addr(addr)
+        process = _ping(host)
+        process.wait()
+        return_code = process.returncode
+        match return_code:
+            case 0:
+                results["reachable"] += [host]
+                res_output = f"{host} - Узел доступен"
+            case _:
+                results["unreachable"] += [host]
+                res_output = f"{host} - Узел не доступен"
+        print(res_output)
     return results
 
 
