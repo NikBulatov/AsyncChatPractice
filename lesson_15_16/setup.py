51c98faa32bcbb5b8d05e8163ddca4688c7ff5fc
Nikita Bulatov <nikita.bulatov@sigur.com>
2023-07-27 11:15:03 +0300

    exe setup file

--- lesson_15_16/setup.py
+++ lesson_15_16/setup.py
@@ -1,10 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages  # , setup
+from cx_Freeze import setup, Executable
+
+build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
 
 setup(name="AsyncChatPractice",
-      version="0.1",
+      version="0.1.1",
       description="A Simple AsyncChatPractice Package",
       author="GB's student Nikita Bulatov",
       packages=find_packages(),
       author_email="nikita_bulatov@icloud.com",
-      url="https://github.com/NikBulatov/AsyncChatPractice"
+      url="https://github.com/NikBulatov/AsyncChatPractice",
+      options={
+          "build_exe": build_exe_options
+      },
+      executables=[Executable("server.py"), Executable("client.py")]
       )
